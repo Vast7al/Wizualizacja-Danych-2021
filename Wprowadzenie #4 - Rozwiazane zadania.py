d12ad4c5cf9f997193f3c7495b03d254b5529d3f
Vast7al (Szymon Świerzbin) <73667837+Vast7al@users.noreply.github.com>
2021-03-30 18:47:41 +0200

    Added zadanie 2.

--- Wprowadzenie #4 - Rozwiazane zadania.py
+++ Wprowadzenie #4 - Rozwiazane zadania.py
@@ -1,12 +1,17 @@
-#import random
-#import sys
-#import math
-lista = []
-for x in range(1, 256, 1):
-    if (x % 4 == 0):
-       lista += [x]
-file=open("podzielne.txt","w+")
+import random
+import sys
+import math
+#zadanie 1.
+#lista = []
+#for x in range(1, 256, 1):
+    #if (x % 4 == 0):
+       #lista += [x]
+#file=open("podzielne.txt","a+")
 
-file.writelines(str(lista))
+#file.writelines(str(lista))
 
-file.close()
+#file.close()
+#zadanie 2.
+file=open("podzielne.txt","r")
+odczyt=file.readlines()
+print(odczyt)

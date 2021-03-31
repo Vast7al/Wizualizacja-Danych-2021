29b1d731a864b110d92fe2f866ed3b53db32e893
Vast7al (Szymon Świerzbin) <73667837+Vast7al@users.noreply.github.com>
2021-03-31 10:57:27 +0200

    Added zadanie  4.

--- Wprowadzenie #4 - Rozwiazane zadania.py
+++ Wprowadzenie #4 - Rozwiazane zadania.py
@@ -24,6 +24,21 @@ import math
     #for text in file:
         #print(text, end="-]")
 #zadanie 4
-
+#class naZakupy:
+   #def __init__(self, nazwa_produktu, ilosc, jednostka_miary, cena_jed):
+        #self.nazwa_produktu =nazwa_produktu
+        #self.ilosc =ilosc
+        #self.jednostka_miary =jednostka_miary
+        #self.cena_jed =cena_jed
+    #def wyswietlenie(self):
+        #print("Nazwa produktu: "+str(self.nazwa_produktu))
+        #print("Ilość: "+str(self.ilosc))
+        #print("Jednostka miary: "+str(self.jednostka_miary))
+        #print("Cena produktu: "+str(self.cena_jed))
+    #def ile_kosztuje(self):
+        #return self.ilosc*self.cena_jed 
+    #def ile_produktu(self):
+        #return str(self.ilosc)+" "+str(self.jednostka_miary)
+       
 
 

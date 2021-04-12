6d98b806edcd4fd1ac4a3222b84e4efa1cbc024d
Vast7al (Szymon Świerzbin) <73667837+Vast7al@users.noreply.github.com>
2021-04-12 21:00:43 +0200

    Added + Updated wprowadzenie 1 (jak moglem o nim zapomniec?)

---
+++ Wprowadzenie #1 - Rozwiazane zadania.py
@@ -0,0 +1,27 @@
+#import sys
+#import math
+#zadanie 1
+#print(math.e**10)
+#print(math.floor(3.55))
+#print(math.ceil(4.80))
+#zadanie 2
+#imie="WOJTEK"
+#nazwisko="SWIERZBIN"
+#print(imie.capitalize(),"",nazwisko.capitalize())
+#zadanie 3.
+#piosenka="la la la la ababa la la la la la la la la la la la la"
+#wartosc=piosenka.count("la")
+#print(wartosc)
+#zadanie 4.
+#gra="The Last of Us"
+#print(gra[1] + gra[-1])
+#zadanie 5.
+#zmienna="Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker"
+#x=zmienna.split()
+#print(x)
+#zadanie 7.
+#imie="Wojciech"
+#liczba_liter1=imie[2]
+#nazwisko="Swierzbin"
+#liczba_liter2=nazwisko[3]
+#print("Tekst zawiera",imie.count(liczba_liter1),"liter",liczba_liter1,"w imieniu Wojciech i",nazwisko.count(liczba_liter2),"liter",liczba_liter2," w nazwisku Swierzbin")

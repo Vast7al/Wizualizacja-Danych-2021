0b8225cbebb2de350b234c8a5342e33abbc1c7f3
Vast7al (Szymon Świerzbin) <73667837+Vast7al@users.noreply.github.com>
2021-04-20 08:17:45 +0200

    Added Wprowadzenie #6 - Rozwiazane zadania

---
+++ Wprowadzenie #6 - Rozwiazane zadania.py
@@ -0,0 +1,9 @@
+import numpy as np
+import sys
+import math
+#zadanie 1
+#Numpy=np.arange(2,44,2)
+#print(Numpy)
+#zadanie 2
+#lista=np.arange["5.6667", "27.876", "41.852","7.81","6.91","5.2","19.8124","58.991"]
+#lista2=lista.astype('int64')

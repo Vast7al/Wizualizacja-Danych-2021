806a3df9bd5886a822c4825d3a83c46f5c9c63bb
Vast7al (Szymon Świerzbin) <73667837+Vast7al@users.noreply.github.com>
2021-04-20 20:21:06 +0200

    Wprowadzenie #6 - Rozwiazane zadania
    Updated.

--- Wprowadzenie #6 - Rozwiazane zadania.py
+++ Wprowadzenie #6 - Rozwiazane zadania.py
@@ -7,3 +7,17 @@ import math
 #zadanie 2
 #lista=np.arange["5.6667", "27.876", "41.852","7.81","6.91","5.2","19.8124","58.991"]
 #lista2=lista.astype('int64')
+#zadanie 3
+#def funkcja(n):
+    #n=int(input(" Podaj wartosc n "))
+    #Numpy=np.arange(1, n*n+1).reshape((n, n))
+    #print(Numpy)
+#n=int
+#funkcja(n)
+#zadanie 5
+#def wektor(n):
+    #return np.diagram(range(n, 0, -1))
+
+#n=int(input(" Podaj wartosc n "))
+#print(wektor(n))
+
